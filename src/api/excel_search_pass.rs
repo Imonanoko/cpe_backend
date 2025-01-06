17ffa291eb31d4cb15e270774240f78fff393e16
six square <Imonanoko@gmail.com>
2025-01-06 06:06:59 +0000

    excel 學號查詢完成

--- src/api/excel_search_pass.rs
+++ src/api/excel_search_pass.rs
@@ -1,16 +1,22 @@
 use actix_multipart::Multipart;
 use actix_session::Session;
-use actix_web::{post, HttpRequest, HttpResponse};
+use actix_web::{post, web, HttpRequest, HttpResponse};
+use calamine::DataType;
+use calamine::Reader;
 use futures_util::StreamExt as _;
+use sqlx::MySqlPool;
+use sqlx::Row;
+use std::fs::File;
 use std::io::Write;
+use xlsxwriter::Workbook;
 
 #[post("/api/excel_search_pass")]
 async fn excel_search_pass(
     mut payload: Multipart,
     req: HttpRequest,
     session: Session,
+    db_pool: web::Data<MySqlPool>,
 ) -> HttpResponse {
-    // 验证 CSRF Token
     let csrf_token_header = req
         .headers()
         .get("X-CSRF-Token")
@@ -20,7 +26,6 @@ async fn excel_search_pass(
         return HttpResponse::Forbidden().body("Invalid CSRF Token");
     }
 
-    // 验证 Session
     if session
         .get::<bool>("is_logged_in")
         .unwrap_or(Some(false))
@@ -31,16 +36,14 @@ async fn excel_search_pass(
     }
 
     let temp_filepath = "./uploads/temp_file.xlsx";
-
+    //儲存上傳的檔案
     while let Some(Ok(field)) = payload.next().await {
         let content_disposition = field.content_disposition();
-        if let Some(filename) = content_disposition.and_then(|cd| cd.get_filename()) {
-            println!("Receiving file: {}", filename);
-            let mut f = std::fs::File::create(temp_filepath).expect("Failed to create file");
+        if let Some(_filename) = content_disposition.and_then(|cd| cd.get_filename()) {
+            let mut f = File::create(temp_filepath).expect("Failed to create file");
             let mut field_stream = field;
             while let Some(chunk) = field_stream.next().await {
                 let data = chunk.expect("Error reading chunk");
-                println!("Writing chunk of size: {}", data.len());
                 f.write_all(&data).expect("Error writing chunk");
             }
         }
@@ -50,28 +53,131 @@ async fn excel_search_pass(
         println!("File does not exist: {}", temp_filepath);
         return HttpResponse::InternalServerError().body("Failed to process file");
     }
-    
-    if let Ok(metadata) = std::fs::metadata(temp_filepath) {
-        println!("File metadata: {:?}", metadata);
-    } else {
-        println!("Failed to retrieve file metadata");
+    //解析上傳的檔案，取得學號的list
+    let mut workbook = match calamine::open_workbook_auto(temp_filepath) {
+        Ok(wb) => wb,
+        Err(err) => {
+            println!("Failed to open Excel file: {}", err);
+            return HttpResponse::InternalServerError().body("Invalid Excel file");
+        }
+    };
+
+    let range = match workbook.worksheet_range("工作表1") {
+        Ok(range) => range, // 成功时返回 range
+        Err(err) => {
+            println!("Error reading sheet: {}", err);
+            return HttpResponse::BadRequest().body("Invalid Excel sheet");
+        }
+    };
+
+    let student_ids: Vec<String> = range
+        .rows()
+        .skip(1)
+        .filter_map(|row| row.get(0))
+        .filter_map(|cell| cell.get_string())
+        .map(|s| s.to_string())
+        .collect();
+
+    if student_ids.is_empty() {
+        return HttpResponse::BadRequest().body("No valid Student IDs found in Excel file");
+    }
+    let query = r#"
+    SELECT 
+        si.StudentID AS StudentID, 
+        si.Name AS Name,
+        CAST(COALESCE(SUM(ea.CorrectAnswersCount), 0) AS UNSIGNED INTEGER) AS TotalCorrectAnswers, 
+        CAST(COALESCE(MAX(ea.CorrectAnswersCount), 0) AS UNSIGNED INTEGER) AS MaxCorrectAnswers
+    FROM 
+        StudentInfo si
+    LEFT JOIN 
+        ExamAttendance ea ON si.StudentID = ea.StudentID
+    WHERE 
+        si.StudentID = (?)
+    GROUP BY 
+        si.StudentID, si.Name;
+    "#;
+
+    let mut result: Vec<(String, String, u16, u8)> = Vec::new();
+    for student_id in student_ids.iter() {
+        let rows = match sqlx::query(query)
+            .bind(student_id)
+            .fetch_all(db_pool.get_ref())
+            .await
+        {
+            Ok(rows) => rows,
+            Err(err) => {
+                println!("Database query failed: {}", err);
+                continue;
+            }
+        };
+        // println!("rows: {:#?}", rows);
+        for row in rows {
+            let student_id: String = row.try_get("StudentID").expect("Failed to get StudentID");
+            let name: String = row.try_get("Name").unwrap_or_else(|_| "Unknown".to_string());
+            let total_correct_answers: u16 = row
+                .try_get("TotalCorrectAnswers")
+                .expect("Failed to get TotalCorrectAnswers");
+            let max_correct_answers: u8 = row
+                .try_get("MaxCorrectAnswers")
+                .expect("Failed to get MaxCorrectAnswers");
+            
+            result.push((
+                student_id,
+                name,
+                total_correct_answers,
+                max_correct_answers,
+            ));
+        }
     }
 
+    let output_filepath = "./uploads/result_file.xlsx";
+    let workbook = Workbook::new(output_filepath).expect("Failed to create workbook");
+    let mut worksheet = workbook.add_worksheet(None).unwrap();
 
-    match std::fs::read(temp_filepath) {
-        Ok(file_data) => {
-            println!("Returning file of size: {}", file_data.len());
-            HttpResponse::Ok()
-                .content_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-                .append_header((
-                    "Content-Disposition",
-                    "attachment; filename=processed_file.xlsx",
-                ))
-                .body(file_data)
-        },
+    worksheet.write_string(0, 0, "學號", None).unwrap();
+    worksheet
+        .write_string(0, 1, "姓名", None)
+        .unwrap();
+    worksheet
+        .write_string(0, 2, "累計題數", None)
+        .unwrap();
+    worksheet
+        .write_string(0, 3, "最高題數", None)
+        .unwrap();
+    worksheet.write_string(0, 4, "是否通過", None).unwrap();
+    for (i, (student_id,name, total_correct_answers, max_correct_answers)) in result.iter().enumerate() {
+        worksheet
+            .write_string(i as u32 + 1, 0, student_id, None)
+            .unwrap();
+        worksheet
+            .write_string(i as u32 + 1, 1, &name, None)
+            .unwrap();
+        worksheet
+            .write_number(i as u32 + 1, 2, *total_correct_answers as f64, None)
+            .unwrap();
+        worksheet
+            .write_number(i as u32 + 1, 3, *max_correct_answers as f64, None)
+            .unwrap();
+        let pass = *total_correct_answers>=3 || *max_correct_answers>=2;
+        worksheet
+            .write_string(i as u32 + 1, 4, if pass { "通過" } else { "不通過" }, None)
+            .unwrap();
+    }
+
+    workbook.close().unwrap();
+
+    match std::fs::read(output_filepath) {
+        Ok(file_data) => HttpResponse::Ok()
+            .content_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            .append_header((
+                "Content-Disposition",
+                "attachment; filename=result_file.xlsx",
+            ))
+            .body(file_data),
         Err(err) => {
-            println!("Error reading file: {}", err);
-            HttpResponse::InternalServerError().body(format!("Error reading file: {}", err))
+            println!("Error reading generated file: {}", err);
+            HttpResponse::InternalServerError()
+                .body("Failed to generate or retrieve result Excel file")
         }
     }
 }
